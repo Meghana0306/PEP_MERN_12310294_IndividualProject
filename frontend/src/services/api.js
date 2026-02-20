e7b0db693714148b42f29a90307514da91073a96
Meghana <meghana7182@gmail.com>
2026-02-20 23:34:08 +0530

    chore(api): set axios request timeout to 15s

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,6 +5,7 @@ const API_BASE_URL = rawApiBaseUrl.replace(/\/$/, "");
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
+  timeout: 15000,
   headers: {
     "Content-Type": "application/json",
   },
